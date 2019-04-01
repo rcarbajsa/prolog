59d4099fc7a2c42af8ae7f43a30553cba84921fa
rcarbajsa <raulcarbajosa7@hotmail.com>
2019-04-01 19:03:06 +0200

    Implementada alturaTorre/2

--- a.pl
+++ a.pl
@@ -1,7 +1,9 @@
 less(0,s(X)):-nat(X).
 less(0,0).
 less(s(X),s(Y)):-less(X,Y).
-nat(0):-true.
+eq(s(X),s(Y)):-eq(X,Y).
+eq(0,0).
+nat(0).
 nat(s(X)):-nat(X).
 dim(s(X)):-dim(X).
 dim(s(0)).
@@ -14,6 +16,13 @@ construccion( [pieza(H,J,O,P)|B] ):-pieza(H,J,O,P),construccion(B).
 esTorre(construccion([G|K])):-
 	esMayor(G,construccion(K)),construccion([G|K]).
 esMayor(pieza(X,Y,Z,C),construccion([pieza(Q,W,E,R)|K])):-
-	less(Q,X),less(E,Z),esMayor(pieza(Q,W,E,R),construccion(K)),pieza(X,Y,Z,C),pieza(Q,W,E,R).
-esMayor(H,construccion([])).
+	less(X,Q),less(Z,E),esMayor(pieza(Q,W,E,R),construccion(K)),pieza(X,Y,Z,C),pieza(Q,W,E,R).
+esMayor(_,construccion([])).
 construccion([]).	
+alturaTorre(construccion(T),A):-esTorre(construccion(T)),nat(A),sumaAltura(construccion(T),A).
+sumaAltura(construccion([S|B]),A):-sumRec(S,construccion(B),0,A).
+sumRec(pieza(X,Y,Z,C),construccion([pieza(Q,W,E,R)|K]),P,A):-
+	sum(Y,P,J),sumRec(pieza(Q,W,E,R),construccion(K),J,A),pieza(X,Y,Z,C),pieza(Q,W,E,R).
+sumRec(pieza(X,Y,Z,C),construccion([]),P,A):-pieza(X,Y,Z,C),sum(Y,P,J),eq(J,A).
+sum(0,Y,Y):-nat(Y).
+sum(s(X),Y,s(Z)):-sum(X,Y,Z).
