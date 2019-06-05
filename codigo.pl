194736ed506e1ce1a1bf9c2f0858c9969b92385c
Alexsente <32201052+Alexsente@users.noreply.github.com>
2019-06-05 11:49:56 +0200

    menor_o_igual supuestamente bien

--- codigo.pl
+++ codigo.pl
@@ -1,11 +1,13 @@
-%Menor devuelve A o B según en M según Comp
+
+	%Menor devuelve A o B según en M según Comp
 
 menor(A,B,Comp,M):-
 	C=..[Comp,A,B],
-	call(C),!, % Si se cumple call(C) ya no se comprueban más opciones
+	call(C), % Si se cumple call(C) ya no se comprueban más opciones
 	M=A.
-menor(_,B,_,B):-
-	A\=B.
+menor(A,B,_,B):-
+	A\=B.%En el caso de que no se cumpla call(C) menor devuelve B
+
 % menor_o_igual comprueba si X es menor o igual a Y
 % menor_o_igual(X,Y)
 
@@ -17,13 +19,13 @@ menor_o_igual(X,X).
 menor_o_igual(X,Y):-
 	functor(X,N1,_),
 	functor(Y,N2,_),
-	N1 @< N2.
+	menor(N1,N2,@<,X).
 menor_o_igual(X,Y):-
 	compound(X),
 	compound(Y),
 	functor(X,N,A1),
 	functor(Y,N,A2),
-	A1 < A2.
+	menor(A1,A2,<,A1).
 menor_o_igual(X,Y):-
 	compound(X),
 	compound(Y),
@@ -35,9 +37,16 @@ recArg(X,Y,A):-
 	arg(A,X,C1),
 	arg(A,Y,C2),
 	A1 is A -1,
-	menor_o_igual(C1,C2),
-	recArg(X,Y,A1).
-	
+	recArg(X,Y,A1),
+	\+menor_o_igual(C1,C2).
+recArg(_,_,_).
+
+
+
+
+
+
+
 lista_hojas(L,H):-
 	findall(tree(X,void,void),(member(X,L)),H).
 
