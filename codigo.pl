083d9818a3d65e42d16f07af562bf9b718a36e85
Raul <rcarbajso@gmail.com>
2019-06-07 18:26:00 +0200

    Funciona ordenacion con numeros

--- codigo.pl
+++ codigo.pl
@@ -1,21 +1,25 @@
 
-	%Menor devuelve A o B según en M según Comp
+%Predicado alumno_prode(Apellido1, Apellido2, Nombre, NumMatricula)
+alumno_prode('Senovilla', 'Tejedor', 'Alejandro', '160172').
+alumno_prode('Carbajosa', 'Gonzalez', 'Raul', '160311').
+alumno_prode('Gonzalez', 'Jurado', 'Gema Maria', '160133').
 
+
+%menor(A,B,Comp,M): Devuelve A o B según Comp en M.
+%Comp debe ser <.
 menor(A,B,Comp,M):-
 	C=..[Comp,A,B],
 	call(C), % Si se cumple call(C) ya no se comprueban más opciones
 	M=A.
 menor(A,B,_,B):-
-	A\=B.%En el caso de que no se cumpla call(C) menor devuelve B
+	A\=B. %En el caso de que no se cumpla call(C) menor devuelve B
 
-% menor_o_igual comprueba si X es menor o igual a Y
-% menor_o_igual(X,Y)
-
+% menor_o_igual(X,Y): Comprueba si X es menor o igual a Y
 menor_o_igual(X,_):-
 	var(X). %En el caso de que A o B sea una variable libre => A es igual a B
 menor_o_igual(_,Y):-
 	var(Y).
-menor_o_igual(X,X).
+menor_o_igual(X,X). %Caso base.
 menor_o_igual(X,Y):-
 	functor(X,N1,_),
 	functor(Y,N2,_),
@@ -32,7 +36,7 @@ menor_o_igual(X,Y):-
 	functor(X,N,A),
 	functor(Y,N,A),
 	recArg(X,Y,A).
-recArg(_,_,0).
+recArg(_,_,0). %Caso base.
 recArg(X,Y,A):-
 	arg(A,X,C1),
 	arg(A,Y,C2),
@@ -41,24 +45,22 @@ recArg(X,Y,A):-
 	\+menor_o_igual(C1,C2).
 recArg(_,_,_).
 
-
-
-
-
-
-
+%lista_hojas(Lista,Hojas): Dada una lista,
+%te devuelve una lista de hojas de arbol(tree(_,void,void)).
 lista_hojas(L,H):-
 	findall(tree(X,void,void),(member(X,L)),H).
 
-hojas_arbol([L|[]],_,L).
+%hojas_arbol(Hojas,Comp,Arbol): Dada una lista de hojas,
+%devuelve un arbol flotante creado con dichas hojas.
+hojas_arbol([L|[]],_,L). %Caso base.
 hojas_arbol(L,C,A):-
 	recA(L,C,_,Res),
 	hojas_arbol(Res,C,A).
-recA([],_,R,R).
+recA([],_,R,R). %Caso base.
 recA([X|[Y|Z]],C,A,Res):-
 	X=..[_,V,_,_],
 	Y=..[_,W,_,_],
-	menor(V,W,C,M),
+	menor(V,W,C,M),!,
 	(A=[] ->append([tree(M,X,Y)],A,R);
 	append([tree(M,X,Y)],A,R)) ,
 	recA(Z,C,R,Res).
@@ -66,7 +68,9 @@ recA([X|[]],_,R,Res):-
 	append([X],R,Z),
 	recA([],_,Z,Res).
 
-ordenacion(void,_,[]).
+%ordenacion(Arbol,Comp,Orden): Dado un arbol, mediante un Comp,
+%devuelve una lista Orden con las hojas de dicho arbol.
+ordenacion(void,_,[]). %Caso base
 ordenacion(Arbol,Comp,Orden):-
 	ordenacionR(Arbol,Comp,[],Orden).
 
@@ -77,7 +81,7 @@ ordenacionR(Arbol,Comp,Acc,Orden):-
 	N=[V|Acc],
 	borrar_hoja(Arbol,ArbolR,V,_,_),
 	obtener_hojas(ArbolR,_,Hojas),
-	hojas_arbol(Hojas,Comp,Res),
+	hojas_arbol(Hojas,Comp,Res),!,
 	ordenacionR(Res,Comp,N,Orden).
 	
 
@@ -94,7 +98,7 @@ borrar_hoja(tree(Z,H1,H2),A,V,Encontrado,0):-
 borrar_hoja(Arbol,Arbol,_,0,1).
 
 
-obtener_hojas(void,Res,Res).	
+obtener_hojas(void,Res,Res). %Caso base.
 obtener_hojas(tree(V,void,void),Hojas,Res):-
 	A=tree(V,void,void),
 	(Hojas=[]->append([A],Hojas,Res);
@@ -103,7 +107,15 @@ obtener_hojas(tree(_,H1,H2),Hojas,Res):-
 	obtener_hojas(H1,Hojas,ResI),
 	obtener_hojas(H2,ResI,Res).
 
+%ordenar(Lista,Comp,Orden): Dada una lista de elementos y un Comp,
+%devuelve la lista ordenada pero utilizando arboles flotantes
+%(Llamando a los metodos creados anteriormente).
 ordenar(Lista,Comp,Orden):-
 	lista_hojas(Lista,Hojas),
-	hojas_arbol(Hojas,Comp,Arbol),
-	ordenacion(Arbol,Comp,Orden).
+	hojas_arbol(Hojas,Comp,Arbol),!,
+	ordenacion(Arbol,Comp,Orden) .
+
+
+push(V,[],[V]).
+push(V,[L|R],[L|T]):-
+	push(V,R,T).
