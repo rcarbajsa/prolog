0314eeb874dfc33820c4b64189b9c2446e33ab3f
Raul <rcarbajso@gmail.com>
2019-06-09 11:34:41 +0200

    Ya están añadidos los comentarios

--- codigo.pl
+++ codigo.pl
@@ -11,8 +11,8 @@ menor(A,B,Comp,M):-
 	C=..[Comp,A,B],
 	call(C), % Si se cumple call(C) ya no se comprueban más opciones
 	M=A,!.
-menor(A,B,_,B):-
-	A\=B.%En el caso de que no se cumpla call(C) menor devuelve B
+menor(A,B,_,B):-%En el caso de que no se cumpla call(C) menor devuelve B
+	A\=B.%En el caso de que A sea igual que B y no se haya cumplido el call, menor devuelve no. Ej: ? menor(3,3,<,M). devuelve no
 
 
 % menor_o_igual comprueba si X es menor o igual a Y
@@ -26,15 +26,15 @@ menor_o_igual(X,X).
 menor_o_igual(X,Y):-
 	functor(X,N1,_),
 	functor(Y,N2,_),
-	N1@<N2.
+	N1@<N2. %Comparamos el functor de los 2 términos
 menor_o_igual(X,Y):-
 	functor(X,N,A1),
 	functor(Y,N,A2),
-	A1<A2.
+	A1<A2. %Comparamos la aridad de los 2 términos
 menor_o_igual(X,Y):-
 	functor(X,N,A),
 	functor(Y,N,A),
-	recArgMenor(X,Y,A).
+	recArgMenor(X,Y,A). %Recorremos los argumentos de los 2 términos y los vamos comparando
 recArgMenor(_,_,0).
 recArgMenor(X,Y,A):-
 	A > 0,
@@ -70,8 +70,9 @@ recArgMayor(X,Y,A):-
 	arg(A,Y,C2),
 	mayor_o_igual(C1,C2).
 
+%comp utiliza menor_o_igual o mayor_o_igual (dependiendo de C) y devuelve el menor o mayor número en M
 comp(A,B,C,M):-
-	(C = 'menor_o_igual'; C = '<' ; C = '=<' ; C = '@<' -> Comp = menor;
+	(C = 'menor_o_igual'; C = '<' ; C = '=<' ; C = '@<'; C= '@=<' -> Comp = menor;
 	Comp = mayor),
 	(Comp = menor -> (menor_o_igual(A,B) -> M = A ;
 			     M = B);
@@ -102,24 +103,31 @@ ordenacion(void,_,[]). %Caso base
 ordenacion(Arbol,Comp,Orden):-
 	ordenacionR(Arbol,Comp,[],Orden),!.
 
-ordenacionR(void,_,Orden,Orden).
+ordenacionR(void,_,Orden,Orden). %Caso base: Cuando ya no quedan hojas en el árbol. Devuelve en Orden los valores que hemos ido acumulando
 ordenacionR(Arbol,Comp,Acc,Orden):-
 	Arbol=tree(V,_,_),
-	push(V,Acc,N),
-	reflotar(Arbol,ArbolR,V,_,_,Comp),!,
+	push(V,Acc,N), %Metemos el valor de la raíz en la lista Acc
+	reflotar(Arbol,ArbolR,V,_,_,Comp),!, %Reflotamos el árbol
 	ordenacionR(ArbolR,Comp,N,Orden).
 	
+%reflotar(Arbol,ArbolR,Valor,Encontrado1,Encontrado2,Comp):
+%Arbol: Árbol/nodo que hay que reflotar
+%ArbolR: Árbol reflotado a partir de Arbol
+%Valor: Valor del nodo raíz, que es el valor de la hoja más pequeña(o mayor)
+%Encontrado1: Devuelve 1 si se ha encontrado la hoja más pequeña(o mayor) en Arbol, devuelve 0 e.o.c
+%Encontrado2: Vale 1 si ya se ha encontrado la hoja más pequeña(o mayor) en algún nodo del árbol, devuelve 0 e.o.c
+%Comp: Criterio de comparación que se utiliza para construir el nuevo árbol.
 
-reflotar(tree(Valor,void,void),void,Valor,1,0,_).
-reflotar(tree(V,H1,H2),A,V,Encontrado,0,C):-
+reflotar(tree(Valor,void,void),void,Valor,1,0,_). %En el caso de que encontremos una hoja con el Valor de la raíz borramos esa hoja (Ponemos ese nodo a void)
+reflotar(tree(V,H1,H2),A,V,Encontrado1,0,C):- %Si aún no se ha encontrado la hoja más pequeña(o mayor) (Encontrado2 = 0) y el valor de este nodo tiene el valor de la raíz buscamos en este nodo. 
 	reflotar(H1,ArbolI,V,E1,_,C),
-	reflotar(H2,ArbolD,V,E2,E1,C),
-	(E1=1 -> Encontrado=1;
-	Encontrado=E2),
+	reflotar(H2,ArbolD,V,E2,E1,C), 
+	(E1=1 -> Encontrado1=1;
+	Encontrado1=E2), %Devolvemos en Encontrado1 si se ha encontrado la hoja con el valor de la raíz en este nodo
 	(ArbolI = void -> A = ArbolD;
-	ArbolD = void -> A = ArbolI;
-	ArbolI = tree(VI,_,_),ArbolD = tree(VD,_,_),comp(VI,VD,C,R),A=tree(R,ArbolI,ArbolD)).
-reflotar(Arbol,Arbol,_,0,_,_).
+	ArbolD = void -> A = ArbolI; %En el caso de que se haya borrado alguna hoja, la hoja que no se ha borrado sustituye al nodo padre
+	ArbolI = tree(VI,_,_),ArbolD = tree(VD,_,_),comp(VI,VD,C,R),A=tree(R,ArbolI,ArbolD)). %Añadimos el nuevo valor del nodo padre que se obtiene con el predicado comp
+reflotar(Arbol,Arbol,_,0,_,_). %Si el nodo no tiene el valor de la raíz lo dejamos como estaba
 
 %ordenar(Lista,Comp,Orden): Dada una lista de elementos y un Comp,
 %devuelve la lista ordenada pero utilizando arboles flotantes
@@ -129,7 +137,7 @@ ordenar(Lista,Comp,Orden):-
 	hojas_arbol(Hojas,Comp,Arbol),
 	ordenacion(Arbol,Comp,Orden),! .
 
-
+%Predicado para almacenar valores en una lista
 push(V,[],[V]).
 push(V,[L|R],[L|T]):-
 	push(V,R,T).
